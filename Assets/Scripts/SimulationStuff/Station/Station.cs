6170216a20a2e0ade0da19ca3afa3d0e6c880f7e
Andrei Patea <andreipatea@ymail.com>
2021-12-14 21:19:33 +0000

    Tryna do programming offline

---
+++ Assets/Scripts/SimulationStuff/Station/Station.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Station : MonoBehaviour
+{
+    public string stationName;
+    public float difficultyValue;
+
+    public void GenerateName()
+    {
+
+    }
+}
