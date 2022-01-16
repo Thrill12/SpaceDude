8241b2ff031bddefaba6d8ae58f3fc20be1201fd
Jared <willj893@gmail.com>
2022-01-16 08:40:08 +0000

    genertor generating not much

---
+++ Assets/Scripts/StationGeneration/TriangulationGraph.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangulationGraph
+{
+
+   void CreateSuperTriangle(List<Vector2Int> verts)
+    {
+        //The 'verts' represent rooms which ther super traingle needs to to enclose. 
+        //Create a triangle which has a point (0,0), (gridsize + 1, 0), (1/2(gridsize), gridsize + 1)
+    }
+}
